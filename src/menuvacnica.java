4b0c0ae539551b3e66c64591211b7ad79f1f09ba
iki55 <aleksovska.isidora@gmail.com>
2023-11-23 12:41:05 +0100

    first commit

---
+++ src/menuvacnica.java
@@ -0,0 +1,15 @@
+
+public class menuvacnica {
+
+	public static void main(String[] args) {
+	double x=23.7;
+	double e=61.7;
+	double rezultat=x*e;
+	System.out.println( "rezultatot iznesuva");
+	System.out.println (rezultat);
+	
+	
+
+	}
+
+}
